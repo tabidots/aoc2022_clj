0dbe6a9bbbc0a9fc71a73f8dbfcaad5d1458db06
Justin Douglas <tabidots@gmail.com>
2022-12-03 20:09:04 +0700

    Initial commit

---
+++ test/tabidots/aoc2022_clj_test.clj
@@ -0,0 +1,7 @@
+(ns tabidots.aoc2022-clj-test
+  (:require [clojure.test :refer :all]
+            [tabidots.aoc2022-clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
